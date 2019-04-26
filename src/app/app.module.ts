df84f441c7d71c4a6d723497b71670af48b0b55e
MarekStenzel <stenzelmarek@gmail.com>
2019-04-26 11:25:47 +0200

    Add sidebar menu and reactive view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RightMenuComponent } from './components/side-menu/right-menu/right-menu.component';
+import { LeftMenuComponent } from './components/side-menu/left-menu/left-menu.component';
+import { MaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightMenuComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AppComponent]
 })
 export class AppModule { }
